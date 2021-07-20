c0a23133f826a55310afc5c3a1bcb739fe65ef0c
Chroax <77435642+Chroax@users.noreply.github.com>
2021-07-21 01:59:05 +0700

    git commit -m "update the Main"

--- src/avanlon/Main.java
+++ src/avanlon/Main.java
@@ -11,8 +11,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int a = 6 / 4;
-        System.out.println(a);
         SwingUtilities.invokeLater(new Runnable() {
 
             @Override
